428bbf404c8cb3d9ebb82b900f6e4d2d5d92c186
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-17 13:32:15 -0400

    Make input search work

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-$(function(){
-const appState={
-    results:[],
-};
-const BASE_URL = "https://www.googleapis.com/youtube/v3/search";
+$(function() {
+  const appState = {
+    results: [],
+  };
+  const BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 
-///////MOD FUNCTIONS////////////
+  ///////MOD FUNCTIONS////////////
 
-//$AJAX
-function getDataFromApi(searchTerm, callback) {
-  const query = {
-    part: 'snippet',
-    key: "AIzaSyCnv7nU_5Tk2WKnDn9re23xzNilS-i9-_M",
-    q: searchTerm
+  //$AJAX
+  function getDataFromApi(searchTerm, callback) {
+    const query = {
+      part: 'snippet',
+      key: "AIzaSyCnv7nU_5Tk2WKnDn9re23xzNilS-i9-_M",
+      q: searchTerm
+    }
+    $.getJSON(BASE_URL, query, callback)
   }
-  $.getJSON(BASE_URL, query, callback)
-}
 
-//////RENDER FUNCTIONS//////////
-function displayResults(data) {
-  console.log('success', data);
-  let resultElement = '';
-  if(data.items) {
-    console.log("are we doing this?")
-    data.items.forEach(function(item) {
-      resultElement += `<img src="${item.snippet.thumbnails.high.url}"/>`;
+  //////RENDER FUNCTIONS//////////
+  function displayResults(data) {
+    console.log('success', data);
+    let resultElement = '';
+    if (data.items) {
+      console.log("are we doing this?")
+      data.items.forEach(function(item) {
+        resultElement += `<img src="${item.snippet.thumbnails.medium.url}"/>`;
 
-    });
+      });
 
+    } else {
+      resultElement = '<p>no result</p>';
+    }
+    console.log(resultElement);
+    $('.js-results').html(resultElement);
   }
-  else {
-    resultElement = '<p>no result</p>';
-  }
-      console.log(resultElement);
-  $('.js-results').html(resultElement);
-}
 
-getDataFromApi('dog video', displayResults);
+  // getDataFromApi('dog video', displayResults);
 
-/////EVENT LISTENERS////////////
-// $(".js-dog-search").submit(function(event) {
-//   event.preventDefault();
-//
-// })
-//
- });
+  ///EVENT LISTENERS////////////
+  $(".js-search-form").submit(function(event) {
+    event.preventDefault();
+    const query = $(this).find('.js-query').val();
+    console.log(query);
+    getDataFromApi(query, displayResults);
+  });
+});
