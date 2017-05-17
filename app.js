48839cab54cb1dc54b803c9d21a4814d85bfb8f4
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-17 15:07:44 -0400

    Make data go to application state

--- app.js
+++ app.js
@@ -7,62 +7,61 @@ $(function() {
   ///////MOD FUNCTIONS////////////
 
   //$.getJSON////////////
-  function getDataFromApi(searchTerm, callback) {
+  function getDataFromApi(searchTerm, state) {
     const query = {
       part: 'snippet',
       key: "AIzaSyCnv7nU_5Tk2WKnDn9re23xzNilS-i9-_M",
       q: searchTerm
     }
-    $.getJSON(BASE_URL, query, callback)
-  }
- 
- function getResults(data) {
-    console.log('success', data);
-    let resultThumbnail = '';
-    let resultId = '';
-    let resultObj = {};
-    if (data.items) {
-      console.log("are we doing this?")
-      data.items.forEach(function(item) {
-        resultThumbnail = item.snippet.thumbnails.medium.url;
-        resultId = item.id.videoId;
-        resultObj = {
+    $.getJSON(BASE_URL, query, function(data) {
+      console.log('success', data);
+      let resultThumbnail = '';
+      let resultId = '';
+      let resultObj = {};
+      if (data.items) {
+        console.log("are we doing this?")
+        data.items.forEach(function(item) {
+          resultThumbnail = item.snippet.thumbnails.medium.url;
+          resultId = item.id.videoId;
+          resultObj = {
             thumbnail: resultThumbnail,
             id: resultId
-        }
-      });
+          }
+          state.results.push(resultObj);
+        });
+        console.log(state);
+      }
+    });
+  };
+
+
+  // function getResults(state, data) {
+  //    console.log('success', data);
+  //    let resultThumbnail = '';
+  //    let resultId = '';
+  //    let resultObj = {};
+  //    if (data.items) {
+  //      console.log("are we doing this?")
+  //      data.items.forEach(function(item) {
+  //        resultThumbnail = item.snippet.thumbnails.medium.url;
+  //        resultId = item.id.videoId;
+  //        resultObj = {
+  //            thumbnail: resultThumbnail,
+  //            id: resultId
+  //        }
+  //        state.question.push(resultObj);
+  //      });
+  //    }
+  //  };
 
   //////RENDER FUNCTIONS//////////
-  function getResults(data) {
-    console.log('success', data);
-    let resultThumbnail = '';
-    let resultId = '';
-    let resultObj = {};
-    if (data.items) {
-      console.log("are we doing this?")
-      data.items.forEach(function(item) {
-        resultThumbnail = item.snippet.thumbnails.medium.url;
-        resultId = item.id.videoId;
-        resultObj = {
-            thumbnail: resultThumbnail,
-            id: resultId
-        }
-      });
 
-    } else {
-      resultElement = '<p>no result</p>';
-    }
-    console.log(resultElement);
-    $('.js-results').html(resultElement);
-  }
-
-  // getDataFromApi('dog video', displayResults);
 
   ///EVENT LISTENERS////////////
   $(".js-search-form").submit(function(event) {
     event.preventDefault();
     const query = $(this).find('.js-query').val();
     console.log(query);
-    getDataFromApi(query, displayResults);
+    getDataFromApi(query, appState);
   });
 });
