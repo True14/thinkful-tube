52e1607d74efdf2988d37eed0295774e4005e2c6
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-17 12:29:48 -0400

    Make first attempt

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
+$(function(){
 const appState={
     results:[],
 };
-
+const BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 
 ///////MOD FUNCTIONS////////////
 
 //$AJAX
-
+function getDataFromApi(searchTerm, callback) {
+  const query = {
+    part: 'snippet',
+    key: "AIzaSyCnv7nU_5Tk2WKnDn9re23xzNilS-i9-_M",
+    q: searchTerm
+  }
+  $.getJSON(BASE_URL, query, callback)
+}
 
 //////RENDER FUNCTIONS//////////
+function displayResults(data) {
+  let resultElement = '<p>No results</p>';
+  if(data.Search) {
+    data.Search.forEach(function(item) {
+      resultElement += `<img src="${items.snippet.thumnails.high.url}/>"`;
+    });
+  }
+  $('.js-results').html(resultElement);
+  console.log('success', data);
+}
 
-
+getDataFromApi('dog video', displayResults);
 
 /////EVENT LISTENERS////////////
+// $(".js-dog-search").submit(function(event) {
+//   event.preventDefault();
+//
+// })
+//
+ });
